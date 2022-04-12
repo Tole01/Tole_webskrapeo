eae4449182d49ca72b197f85577eb8f7ba124790
Luis David Tolentino Barrera <luisdavidtolentino@gmail.com>
2022-04-11 19:01:59 -0500

    a ver si asi

--- scraper.py
+++ scraper.py
@@ -3,12 +3,12 @@ import lxml.html as html
 import os
 import datetime
 
-HOME_URL = 'https://www.larepublica.co/'
+HOME_URL= 'https://vanguardia.com.mx/'
 
-XPATH_LINK_TO_ARTICLE = '//h2/a/@href'
-XPATH_TITLE = '//h2/span/text()'
-XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
-XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/text()'
+XPATH_LINK_TO_ARTICLE= '//div[@class="text_block"]/div[@class="headline"]/a/@href'
+XPATH_TITLE = '//div[@class="container"]/h1[@class="headline artit font-1 medium bold"]/text()'
+XPATH_SUMMARY='//div[@class="row middle"]/div[@class="col-60-40-top-one-col col-xs-12 col-md-9 col-lg-9 portlet-column nopadding"]/h2/text()'
+XPATH_BODY='//div[@class="text resizable"]/div[@class="paragraph texto"]/p/text()'
 
 def parse_notice(link, today):
     try:
@@ -19,7 +19,7 @@ def parse_notice(link, today):
 
             try:
                 title = parsed.xpath(XPATH_TITLE)[0]
-                title = title.replace('/"', '')
+                title = title.replace('\"', '')
                 summary = parsed.xpath(XPATH_SUMMARY)[0]
                 body = parsed.xpath(XPATH_BODY)
             except IndexError:
@@ -27,12 +27,12 @@ def parse_notice(link, today):
 
             with open(f'{today}/{title}.txt' , 'w' , encoding = 'utf-8') as f:
                 f.write(title)
-                f.write('/n/n')
+                f.write('\n\n')
                 f.write(summary)
-                f.write('/n/n')
+                f.write('\n\n')
                 for p in body:
                     f.write(p)
-                    f.write('/n')
+                    f.write('\n')
 
         else:
             raise ValueError(f'Error: {response.status_code}')
@@ -46,7 +46,8 @@ def parse_home():
             home = response.content.decode('utf-8')
             parsed =html.fromstring(home)
             links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
-            #print(links_to_notices)
+            links_to_notices=["https://vanguardia.com.mx"+ y for y in links_to_notices]
+            #a ver si asi
 
             today = datetime.date.today().strftime('%d-%m-%Y')
             if not os.path.isdir(today):
@@ -64,3 +65,7 @@ def run():
 
 if __name__ == '__main__':
     run()
+    
+    
+    
+    
